eb6fc6bdcd90b118a4110f6d5db02eccb2274839
KKS456 <kanhaiyakumar1995@outlook.com>
2022-05-09 17:13:47 +0530

    gew correction made

--- login.js
+++ login.js
@@ -1,17 +1,15 @@
-var attempt = 3;        //no of attempt
-
+var attempt = 3;       
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "12345" && password == "12345"){
 alert ("Login successfully");
-window.location = "navtab.html";              //navigate to other page after login 
+window.location = "navtab.html";              
 return false;
 }
 else{
-attempt --;// Decrementing by one.
+attempt --;
 alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
 if( attempt == 0){
 document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
